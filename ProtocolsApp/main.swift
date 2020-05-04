86c3c3f18be0b6afbab9eaacf148b2903d3d63f4
Guy <guyskv@gmail.com>
2020-05-04 20:08:29 +0300

    Initial Commit

---
+++ ProtocolsApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProtocolsApp
+//
+//  Created by Guy Macin on 04/05/2020.
+//  Copyright © 2020 Guy Macin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
